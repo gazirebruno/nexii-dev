c77093520cae8765dc5815ea2915d5426780a139
gazirebruno <142175978+gazirebruno@users.noreply.github.com>
2024-04-24 14:25:02 -0700

    First deployment

---
+++ app/ui/nexii-logo.tsx
@@ -0,0 +1,12 @@
+export default function background() {
+  return (
+    <div
+      className="flex min-h-screen flex-col p-6"
+      style={{
+        backgroundImage: `url('/NexiiLogo.png')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    ></div>
+  );
+}
